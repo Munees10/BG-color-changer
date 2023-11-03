203a8033b3bbb3a14c264b1681d50760ab72aaf6
MUNEES <Munees10>
2023-11-03 20:48:21 +0530

    background color changing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState(
+    {
+      width: "100%",
+      height: "100vh",
+      backgroundColor: "#000000"
+    }
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={color} className="d-flex justify-content-center align-items-center ">
+
+      <div style={{ border: "solid black 1px" }} className='bg-light p-3 rounded w-50 d-flex justify-content-between align-items-center'>
+        <button onClick={() => setColor({ ...color, backgroundColor: "#ff0000" })} className='btn fw-bold' style={{ backgroundColor: '#ff0000', }}>RED</button>
+        <button onClick={() => setColor({ ...color, backgroundColor: "#808080" })} className='btn fw-bold' style={{ backgroundColor: '#808080' }}>GRAY</button>
+        <button onClick={() => setColor({ ...color, backgroundColor: "#0000ff" })} className='btn fw-bold' style={{ backgroundColor: '#0000ff' }}>BLUE</button>
+        <button onClick={() => setColor({ ...color, backgroundColor: "#ffff00" })} className='btn fw-bold' style={{ backgroundColor: '#ffff00' }}>YELLOW</button>
+        <button onClick={() => setColor({ ...color, backgroundColor: "#ffffff" })} className='btn fw-bold border' style={{ backgroundColor: '#ffffff' }}>WHITE</button>
+        <button onClick={() => setColor({ ...color, backgroundColor: "#008000" })} className='btn fw-bold' style={{ backgroundColor: '#008000' }}>GREEN</button>
+      </div>
+
+
     </div>
   );
 }
